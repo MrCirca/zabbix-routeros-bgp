90ed9f487483524252b1da33662f128d6c574d4f
root <root@mail.mrcirca.com>
2017-04-05 20:48:19 +0300

    Zabbix script, project v.3.0

---
+++ bgp_peer_find.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOST=$1
+USERNAME=$2
+PASSWORD=$3
+
+API_CALL="/routing/bgp/peer/print\n=status=\n"
+API_ROS_COMMAND="/home/zabbix/externalscripts/apiros.py"
+
+PEERS_STATUS=$(echo -e "$API_CALL" | $API_ROS_COMMAND "$HOST" "$USERNAME" "$PASSWORD")
+PEER_NAMES=$(echo -e "$PEERS_STATUS" | grep -E -o '=name=[^ ]+' | cut -d "=" -f 3 | sed 's,\n, ,g')
+
+PEER_NAMES_JSON="{\n\t\"data\": [\n"
+
+for PEER_NAME in $PEER_NAMES
+do
+	PEER_NAMES_JSON="$PEER_NAMES_JSON\n\t\t{\"{#BGP_PEER_NAME}\": \"$PEER_NAME\"},"
+done
+
+PEER_NAMES_JSON=$(echo -n "$PEER_NAMES_JSON" | head -c '-1')
+
+PEER_NAMES_JSON="$PEER_NAMES_JSON\n\t]\n}"
+
+echo -e "$PEER_NAMES_JSON"
