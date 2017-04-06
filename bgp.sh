fa3dc2aec7af2f1fb5e47ac0e02caf235e4f9e4a
root <geortsir3@gmail.com>
2017-04-07 01:08:49 +0300

    Finished

--- bgp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NAME=$1
-HOSTNAME=$2
-USERNAME=$3
-PASSWORD=$4
-echo -e "/routing/bgp/peer/print\n=count-only=?name=$NAME\n" | /home/zabbix/externalscripts/apiros.py $HOSTNAME $USERNAME $PASSWORD | grep "=ret=" | cut -d "=" -f 3
