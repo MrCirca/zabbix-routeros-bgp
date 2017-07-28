9381a8e1c65789b85aa68c98170e88f5636ec892
MrCirca <geortsir3@gmail.com>
2017-07-28 20:40:24 +0300

    Add status for disbled peers

--- bgp_peer_field.sh
+++ bgp_peer_field.sh
@@ -6,21 +6,22 @@ PEER_NAME=$4
 PEER_FIELD_NAME=$5
 API_CALL="/routing/bgp/peer/print\n?name=$PEER_NAME\n"
 API_ROS_COMMAND=$(cat /etc/zabbix/zabbix_server.conf | grep "^ExternalScripts=" | cut -d "=" -f 2)/apiros.py
-
 PEER_FIELD=$(echo -e "$API_CALL" | "$API_ROS_COMMAND" "$HOST" "$USERNAME" "$PASSWORD" | grep "$PEER_FIELD_NAME" | cut -d "=" -f 3)
 
 if [ "$PEER_FIELD_NAME" == "state" ]; then
 	if [ "$PEER_FIELD" == "established" ]; then
-		echo 3
+		echo 4
 
 	elif [ "$PEER_FIELD" == "active" ]; then
-		echo 2
+		echo 3
 
 	elif [ "$PEER_FIELD" == "opensent" ] || [ "$PEER_FIELD" == "openconfirm" ]; then
-		echo 1
+		echo 2
 
 	elif [ "$PEER_FIELD" == "idle" ]; then
-		echo 0
+		echo 1
+	elif [ "$PEER_FIELD" == "" ]; then
+                echo 0
 	else
 		echo
 	fi
@@ -40,6 +41,7 @@ elif [ "$PEER_FIELD_NAME" == "uptime" ]; then
 	TOTAL_SECONDS=$(echo "$DAYS * 86400 + $HOURS * 3600 + $MINUTES * 60 + $SECONDS" | bc)
 
 	echo $TOTAL_SECONDS
+
 else
 	echo "$FIELD_NAME is not supported"
 fi
